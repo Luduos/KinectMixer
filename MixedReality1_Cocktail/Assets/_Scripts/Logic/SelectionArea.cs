da3ad3cd0d6c8c872dd628d080669f68e66fa1c9
luduos <david.liebemann@gmail.com>
2017-10-22 21:41:06 +0200

    Basics of the SelectionArea Created the Selection-Area-objects with a dummy display region

---
+++ MixedReality1_Cocktail/Assets/_Scripts/Logic/SelectionArea.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public enum SelectionAreaType
+{
+    Fruit,
+    Alcohol,
+    NonAlcohol
+}
+
+[RequireComponent(typeof(BoxCollider))]
+public class SelectionArea : MonoBehaviour {
+
+    [SerializeField]
+    private SelectionAreaType AreaType;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // TODO
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        //TODO
+    }
+}
